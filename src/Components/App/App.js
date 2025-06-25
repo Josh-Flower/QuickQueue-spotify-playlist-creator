f40eb2af6d769ec999cb6498eb543d1400641a94
Josh Flower <josh-flower@outlook.com>
2025-06-25 10:40:41 +0100

    added components and css, started to link components together

---
+++ src/Components/App/App.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import SearchResults from "../SearchResults/SearchResults.js";
+import Playlist from "../Playlist/Playlist.js";
+
+function App() {
+  const [searchResults, setSearchResults] = useState([
+    {
+      name: 'example track name 1',
+      artist: 'example track artist 1',
+      album: 'example track album 1',
+      id: 1,
+    },
+    {
+      name: 'example track name 2',
+      artist: 'example track artist 2',
+      album: 'example track album 2',
+      id: 2,
+    }
+  ]
+  );
+  const [playlistName, setPlaylistName] = useState('Example Playlist Name');
+  const [playlistTracks, setPlaylistTracks] = useState([
+    {
+      name: 'example track name 1',
+      artist: 'example track artist 1',
+      album: 'example track album 1',
+      id: 1,
+    },
+    {
+      name: 'example track name 2',
+      artist: 'example track artist 2',
+      album: 'example track album 2',
+      id: 2,
+    },
+    {
+      name: 'example track name 3',
+      artist: 'example track artist 3',
+      album: 'example track album 3',
+      id: 3,
+    },
+  ]);
+
+  function addTrack(track) {
+    const existingTrack = playlistTracks.find((t) => t.id === track.id);
+    const newTrack = playlistTracks.concat(track);
+
+    if (existingTrack) {
+      console.log('Track already exists')
+    } else {
+      setPlaylistTracks(newTrack);
+    }
+  };
+
+  function removeTrack(track) {
+    const existingTrack = playlistTracks.filter((t) => t.id !== track.id);
+    setPlaylistTracks(existingTrack);
+  };
+
+  return (
+    <div>
+      <h1>
+        Ja<span className={styles.highlight} >mmm</span>ing
+      </h1>
+      <div className={styles.App}>
+        {/* <!-- Add a SearchBar component --> */}
+
+        <div className={styles['App-playlist']}>
+          {/* <!-- Add a SearchResults component --> */}
+          <SearchResults userSearchResults={searchResults} onAdd={addTrack} />
+
+          {/* <!-- Add a Playlist component --> */}
+          <Playlist 
+            playlistName={playlistName} 
+            playlistTracks={playlistTracks}
+            onRemove={removeTrack}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Playlist.module.css";
+import Tracklist from "../TrackList/TrackList.js";
+
+function Playlist(props) {
+  return (
+    <div className={styles.Playlist}>
+      <input defaultValue={"New Playlist"} />
+      {/* <!-- Add a TrackList component --> */}
+      <Tracklist 
+        userSearchResults={props.playlistTracks}
+        onRemove={props.onRemove}
+        isRemoval={true}
+      />
+      <button className={styles['Playlist-save']}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  );
+}
+
+export default Playlist;
